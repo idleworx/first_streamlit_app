e8cb5b891fd61b733ca20862886bbd8c83b550e8
idleworx <1016086+idleworx@users.noreply.github.com>
2022-06-07 19:38:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
 
-streamlit.title("my parents new healthy dinner")
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
